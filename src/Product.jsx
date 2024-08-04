1e16298c0366dc278c9a0286556b0effb41ea8d4
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-08-04 12:29:47 +0530

    final commit

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,9 +1,54 @@
-
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
-  
+  const { productId } = useParams();
 
-  return <>Product</>;
+  const mutation = useMutation({
+    mutationFn: (newProduct) => {
+      return axios.put(
+        `https://dummyjson.com/products/${productId}`,
+        newProduct
+      );
+    },
+  });
+
+  const fetchProductById = async () => {
+    const response = await fetch(`https://dummyjson.com/products/${productId}`);
+    const data = await response.json();
+    return data;
+  };
+
+  const {
+    isLoading,
+    error,
+    data: product,
+  } = useQuery({
+    queryKey: ["product", productId],
+    queryFn: fetchProductById,
+    // staleTime: 10000, WE HAVE SET STALETIME AS DEFAULT IN MAIN.JSX
+  });
+
+  if (isLoading) return <h3 className="p-10 text-7xl">Loading...</h3>;
+  if (error) return <h3 className="p-10 text-7xl">{error.message}</h3>;
+
+  if (mutation.isPending) return <h3 className="p-10 text-7xl">updating...</h3>;
+  if (mutation.isError) return <h3 className="p-10 text-7xl">{mutation.error.message}</h3>;
+
+  return (
+    <>
+      Product: {product.title}
+      {mutation.isSuccess ? <div>Todo added!</div> : null}
+      <button
+        onClick={() => {
+          mutation.mutate({ title: "Updated Product" });
+        }}
+      >
+        Create Product
+      </button>
+    </>
+  );
 };
 
 export default Product;
